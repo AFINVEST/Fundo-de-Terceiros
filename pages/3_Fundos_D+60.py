35bcd2149b610c38a0677b3217f5c33b3ae8da19
AFINVEST <grupobackoffice@afinvest.com.br>
2026-04-09 18:17:06 -0300

    Update 3_Fundos_D+60.py

--- pages/3_Fundos_D+60.py
+++ pages/3_Fundos_D+60.py
@@ -18,7 +18,7 @@ tab1, tab2 = st.tabs([
 
 with tab1:
         render_comparacao_fundos(
-        pasta=r"Z:\Asset Management\Equipe\Lívia\Rascunhos\Site_fundos",
+        pasta=str(BASE_DIR),
         arquivo_xlsx="carteira_fundos_consolidada.xlsx",
         titulo="Fundos D+60",
         abas_permitidas=["HORIZONTE", "JGP Select", "ARX Everest Advisory", "Polo Total", "Absolute Olimpia"]  # opcional
@@ -26,7 +26,7 @@ with tab1:
      
 with tab2:
     render_pesquisa_ativo_por_data(
-        pasta=r"Z:\Asset Management\Equipe\Lívia\Rascunhos\Site_fundos",
+        pasta=str(BASE_DIR),
         arquivo_xlsx="carteira_fundos_consolidada.xlsx",
         abas_permitidas=["HORIZONTE", "JGP Select", "ARX Everest Advisory", "Polo Total", "Absolute Olimpia"]  # <-- somente fundos D+60
     )
