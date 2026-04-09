d1beaa572c862e1b38f5f4f34286a0678eda9656
AFINVEST <grupobackoffice@afinvest.com.br>
2026-04-09 18:16:37 -0300

    Update 2_Fundos_D+30.py

--- pages/2_Fundos_D+30.py
+++ pages/2_Fundos_D+30.py
@@ -18,7 +18,7 @@ tab1, tab2 = st.tabs([
 
 with tab1:
         render_comparacao_fundos(
-        pasta=r"Z:\Asset Management\Equipe\Lívia\Rascunhos\Site_fundos",
+        pasta=str(BASE_DIR),
         arquivo_xlsx="carteira_fundos_consolidada.xlsx",
         titulo="Fundos D+30",
         abas_permitidas=["GERAES 30", "Daycoval Classic 30", "Riza Lotus Plus", "ARX Vinson Advisory", "Sparta Max Advisory", "Polo Crédito Corporativo", "Iridium Titam Advisory", "Porto Seguro Ipê", "Sparta Top Advisory", "Absolute Creta"]  # opcional
@@ -26,7 +26,7 @@ with tab1:
         
 with tab2:
     render_pesquisa_ativo_por_data(
-        pasta=r"Z:\Asset Management\Equipe\Lívia\Rascunhos\Site_fundos",
+        pasta=str(BASE_DIR),
         arquivo_xlsx="carteira_fundos_consolidada.xlsx",
         abas_permitidas=["GERAES 30", "Daycoval Classic 30", "Riza Lotus Plus", "ARX Vinson Advisory", "Sparta Max Advisory", "Polo Crédito Corporativo", "Iridium Titam Advisory", "Porto Seguro Ipê", "Sparta Top Advisory", "Absolute Creta"]  # <-- somente fundos D+30
     )
